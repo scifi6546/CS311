8e2b14c75fab3a403ea007990ee53a6fdd598c75
nick <sciufi6546@protonmail.com>
2019-09-25 21:00:20 -0800

    added files

---
+++ HW03/dp3.cpp
@@ -0,0 +1,27 @@
+// dp3.cpp  SKELETON
+// Glenn G. Chappell
+// 2019-09-24
+//
+// For CS 311 Fall 2019
+// Source for Project 3 Functions
+
+#include "dp3.h"       // For Project 3 prototypes & templates
+
+#include <functional>
+using std::function;
+
+
+void didItThrow(const function<void()> & ff,
+                bool & threw)
+{
+    // TODO: Write this!!!
+}
+
+
+int gcd(int a,
+        int b)
+{
+    return 42;  // Dummy return
+    // TODO: Write this!!!
+}
+
---
+++ HW03/dp3.h
@@ -0,0 +1,110 @@
+// dp3.h  SKELETON
+// Glenn G. Chappell
+// 2019-09-24
+//
+// For CS 311 Fall 2019
+// Header for Project 3 Functions
+
+#ifndef FILE_DP3_H_INCLUDED
+#define FILE_DP3_H_INCLUDED
+
+#include <cstddef>     // For std::size_t
+#include <functional>  // For std::function
+
+
+// **************************************************************** // *
+// Begin DO-NOT-CHANGE section                                      // *
+// Do not alter the lines below                                     // *
+// **************************************************************** // *
+                                                                    // *
+                                                                    // *
+// struct LLNode                                                    // *
+// Linked List node, with client-specified value type               // *
+// Invariants:                                                      // *
+//     Either _next is nullptr, or _next points to an LLNode,       // *
+//      allocated with new, owned by *this.                         // *
+// Requirements on Types:                                           // *
+//     ValType must have a copy ctor and a (non-throwing) dctor.    // *
+template <typename ValType>                                         // *
+struct LLNode {                                                     // *
+    ValType   _data;  // Data for this node                         // *
+    LLNode *  _next;  // Ptr to next node, or nullptr if none       // *
+                                                                    // *
+    // The following simplify creation & destruction                // *
+                                                                    // *
+    // 1- & 2-param ctor                                            // *
+    // _data is set to data (given). _next is set to next, if       // *
+    // given, or nullptr if not.                                    // *
+    // Pre:                                                         // *
+    //     theNext, if passed, is either nullptr, or else it points // *
+    //      to an LLNode allocated with new, with ownership         // *
+    //      transferred to *this.                                   // *
+    explicit LLNode(const ValType & data,                           // *
+                    LLNode * next = nullptr)                        // *
+        :_data(data),                                               // *
+         _next(next)                                                // *
+    {}                                                              // *
+                                                                    // *
+    // dctor                                                        // *
+    // Does delete on _next.                                        // *
+    ~LLNode()                                                       // *
+    { delete _next; }                                               // *
+                                                                    // *
+    // No default ctor, copy, or move.                              // *
+    LLNode() = delete;                                              // *
+    LLNode(const LLNode & other) = delete;                          // *
+    LLNode & operator=(const LLNode & other) = delete;              // *
+    LLNode(LLNode && other) = delete;                               // *
+    LLNode & operator=(LLNode && other) = delete;                   // *
+                                                                    // *
+};  // End struct LLNode                                            // *
+                                                                    // *
+                                                                    // *
+// **************************************************************** // *
+// End DO-NOT-CHANGE section                                        // *
+// Do not alter the lines above                                     // *
+// **************************************************************** // *
+
+
+template <typename ValueType>
+ValueType lookup(const LLNode<ValueType> * head,
+                 std::size_t index)
+{
+	if(head==nullptr){
+		throw std::out_of_range("out of range for linked list");
+	}
+	LLNode<ValueType> *item=head->_next;
+	for(int i=0;i<index;i++){
+		if(item->_next==nullptr){
+			throw std::out_of_range("out of range for linked list");
+		}else{
+			item=item->_next;
+		}
+
+	}
+    return item->_data;  // Dummy return
+    // TODO: Write this!!!
+}
+
+
+// Implementation in source file
+void didItThrow(const std::function<void()> & ff,
+                bool & threw);
+
+
+template <typename FDIter>
+bool checkSorted(FDIter first,
+                 FDIter last)
+{
+    return false;  // Dummy return
+    // TODO: Write this!!!
+}
+
+
+// Implementation in source file
+int gcd(int a,
+        int b);
+
+
+#endif  //#ifndef FILE_DP3_H_INCLUDED
+
