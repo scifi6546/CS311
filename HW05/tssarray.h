ec9d041d6d9ff9692922b7f4da43d2959e72dda5
Nick <scifi6546@protonmail.com>
2019-10-30 16:49:47 -0800

    foo

--- HW05/tssarray.h
+++ HW05/tssarray.h
@@ -2,6 +2,7 @@
 #define TSSARRAY
 #include <stdexcept>
 #include <algorithm>
+#include <cstddef>
 //Nicholas Alexeev
 //purpose:implement tssarray
 //25-08-2019
@@ -10,64 +11,121 @@ template <typename T>
 class TSSArray{
 	public:
 		typedef T value_type;
-		typedef unsigned int size_type;
+		typedef std::size_t size_type;
 		typedef T* iterator;
-		typedef T* const_iterator;
+		typedef const T* const_iterator;
 		//basic garuntee
 		//pre: value_type can be constructed 32 times
 		//post: none
-		TSSArray():_size(0),_alloc_size(_DEFAULT_SIZE),_data(new value_type[_DEFAULT_SIZE]){};
-		TSSArray(size_type s_in):_size(s_in),_alloc_size(s_in),_data(new value_type[s_in]){};
+		explicit TSSArray(size_type s_in=0):_size(s_in),
+			_alloc_size(std::max(_size,size_type(_DEFAULT_SIZE))),_data(new value_type[_alloc_size]){};
 		//strong guarentee
 		//pre: in is a valid TSSArray
 		//post: none
-		TSSArray(TSSArray &in):_size(in.size()),_alloc_size(in.size()),_data(new value_type[in.size()]){
-			std::copy(in.begin(),in.end,begin());
+		TSSArray(TSSArray &in){
+			_size=in.size();
+			_alloc_size=_size;
+			_data = new value_type[in.size()];
+			try{
+				std::copy(in.begin(),in.end(),_data);
+			}catch(...){
+				delete [] _data;
+				throw;
+			}
 		}
 		//strong guarentee
 		//pre: in is a valid TSSArray
 		//post: none
-		TSSArray(const TSSArray &in):_size(in.size()),_alloc_size(in.size()),_data(new value_type[in.size()]){
-			std::copy(in.begin(),in.end,begin());
+		TSSArray(const TSSArray &in){
+			if(this==&in){
+				return;
+			}
+			_size=in.size();
+			_alloc_size=_size;
+			_data = new value_type[in.size()];
+			try{
+				std::copy(in.begin(),in.end(),_data);
+			}catch(...){
+				delete [] _data;
+				throw;
+			}
 		}
 
 		//strong guarentee
 		//pre: in is a valid TSSArray
 		//post: none
-		TSSArray(TSSArray &&in):_size(in.size(),_alloc_size(in.size()),_data(in.begin())noexcept{};
-		//strong guarentee
-		//pre: in is a valid TSSArray
-		//post: none
-		TSSArray(const TSSArray &&in):_size(in.size()),_alloc_size(in.size()),_data(in.begin())noexcept{};
+		TSSArray(TSSArray &&in)noexcept{
+			swap(in);
+		};
 
 		//strong guarentee
 		//pre: in is a valid tssarray
 		//post: none
-		TSSArray operator=(TSSArray &in){
-			TSSArray(in);
+		TSSArray& operator=(TSSArray &in){
+			if(this==&in){
+				return *this;
+			}
+			_data= new value_type[in.size()];
+			try{
+				std::copy(in.begin(),in.end(),begin());
+			}catch(...){
+				delete [] _data;
+				throw;
+			}
+			_alloc_size=in.size();
+			_size=_alloc_size;
+			return *this;
 		}
 		//strong guarentee
 		//pre: in is a valid tssarray
 		//post: none
-		TSSArray operator=(const TSSArray &in){
-			TSSArray(in);
+		TSSArray& operator=(const TSSArray &in){
+			_data= new value_type[in.size()];
+			
+			try{
+				std::copy(in.begin(),in.end(),begin());
+			}catch(...){
+				delete [] _data;
+				throw;
+			}
+			_alloc_size=in.size();
+			_size=_alloc_size;
+			return *this;
 		}
 		//nothrow guarentee
 		//pre: in is a valid tssarray
 		//post: none
-		TSSArray operator=(TSSAray &&in)noexcept{
-			TSSArray(in);
+		TSSArray& operator=(TSSArray &&in)noexcept{
+			swap(in);
+			return *this;
+			/*
+			if(this==&in)
+				return *this;
+			_data=in._data;
+			_alloc_size=in._alloc_size;
+			_size=in._size;
+			in._size=0;
+			in._alloc_size=0;
+			in._data=nullptr;
+			return *this;*/
 		}
-		//no throw guarentee
+		//nothrow guarentee
 		//pre: in is a valid tssarray
 		//post: none
-		TSSArray operator=(const TSSAray &&in)noexcept{
-			TSSArray(in);
-		}
+		/*
+		TSSArray& operator=(const TSSArray &&in)noexcept{
+			_data=in._data;
+			_alloc_size=in._alloc_size;
+			_size=in._size;
+			in._size=0;
+			in._alloc_size=0;
+			in._data=nullptr;
+			return *this;
+		}*/
 		//strong garuentee
 		//pre: i<size()
 		//post: none
-		TSSArray& operator[](size_type i){
+		value_type& operator[](size_type i){
 			if(i<size()){
 				return _data[i];
 			}
@@ -76,7 +134,7 @@ class TSSArray{
 		//strong garuentee
 		//pre: i<size()
 		//post: none
-		const TSSArray& operator[](size_type i){
+		const value_type& operator[](size_type i)const{
 			if(i<size()){
 				return _data[i];
 			}
@@ -85,54 +143,143 @@ class TSSArray{
 		//no throw garuentee
 		//pre: valid array
 		//post: none
-		size_type size()noexcept{
+		const size_type size()const noexcept{
 			return _size;
 		}
 		//no throw garuentee
 		//pre: valid array
 		//post: none
-		bool empty()noexcept{
+		bool empty()const noexcept{
 			return _size==0;
 		}
 
 
-		void resize(int size){
-			if(size>_size){
-
-				if(size<=_alloc_size){
-					for(size_type i=size;i<_alloc_size;i++){
-						_data[i]=value_type();
-					}
-				}else{
-					value_type temp* = new value_type[size];
-					std::copy(begin(),end(),temp);
-					_size=size;
-					_alloc_size=size;
+		//weak gaurentee
+		//pre: size is able to be allocated
+		void resize(size_type size_in){
+			//testing if new size is smaller
+			if(size_in<size()){
+				for(value_type* i=_data+size_in;i<_data+_size;i++){
+					i->~value_type();
 				}
+				_size=size_in;
 			}else{
-				for(size_type i=size;i<_size){
-					_data->~value_type();
+				//if need to resize
+				if(size_in>_alloc_size){
+
+					value_type *temp = new value_type[size_in];
+					std::copy(begin(),end(),temp);
+					_data= temp;
+					_alloc_size=size_in;
+					_size=size_in;
+				//if space is already allocated just need to run ctors
+				}else{
+					for(value_type* i=_data+_size;i<_data+size_in;i++){
+						i = new value_type();
+					}
+					_size = size_in;
 				}
-				_size=size;
+				
 			}
 		}
-		void insert(iterator &iterator,value_type& ins){
+		//weak garuentee
+		//pre: iter in range
+		iterator insert(iterator iter,const value_type &ins){
+			if(iter>end()|| iter<begin()){
+				throw std::out_of_range("iterator out of range");
+			}
+			size_type index=iter-begin();
+			resize(size()+1);
+			std::rotate(begin() + index, end()-1, end());	
+			//std::copy(begin()+index,end(),begin()+index+1);
+			_data[index]=ins;
+
+			return index+begin();
+		}
+		//weak garentee
+		//pre: iter in range
+		iterator erase(iterator elm){
+			if(elm>=begin()&&elm<end()){
+				elm->~value_type();
+				std::copy(elm+1,end(),elm);
+				_size--;
+				return elm;
+			}
+			throw std::out_of_range("iterator out of range");
+		}
+		//weak garentee
+		//pre: data is valid
+		iterator begin()noexcept{
+			return _data;
+		}
+		//weak garentee
+		//pre: data is valid
+		const_iterator begin()const noexcept{
+			return _data;
+		}
+		//weak garentee
+		//pre: data is valid
+		iterator end()noexcept{
+			return _data+_size;
+		}
+		//weak garentee
+		//pre: data is valid
+		const_iterator end()const noexcept{
+			return _data+_size;
+		}
+		//weak gaurentee
+		//pre: data is valid
+		void push_back(const value_type& in){
+			if(_size>=_alloc_size){
+				resize(size()+1);
+				_data[_size-1]=in;
+			}else{
+				_data[_size]=in;
+				_size++;
+			}
 
 		}
+		void pop_back(){
+			if(_size>=1){
+				end()->~value_type();
+				_size--;
+			}
+		}
+		void swap(TSSArray &in)noexcept{
+			if(&in==this)
+				return;
+			T* temp = in._data;
+			size_type t_alloc = in._alloc_size;
+			size_type t_size = in._size;
+
+
+			in._data=_data;
+			in._alloc_size=_alloc_size;
+			in._size=_size;
+
+
+			_data=temp;
+			_alloc_size=t_alloc;
+			_size=t_size;
+		}
+
 		
 
 		~TSSArray(){
-			delete _data;
+			if(_data!=nullptr)
+				delete[] _data;
+
 		}
+
 
 
 
 
 	private:
-		size_type _size;
-		size_type _alloc_size;
-		value_type [] _data;
-		const size_type _DEFAULT_SIZE=0x20;
+		size_type _size=0;
+		size_type _alloc_size=0;
+		value_type * _data=nullptr;
+		enum def_size:size_type{_DEFAULT_SIZE=0x20};
 
-}
+};
 #endif
