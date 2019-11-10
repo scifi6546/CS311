f4eb45f51a0ace9dad34d4aea357dba9b3cd3fd1
Nick <scifi6546@protonmail.com>
2019-11-10 10:39:51 -0900

    done!

--- HW06/dp6.h
+++ HW06/dp6.h
@@ -1,7 +1,13 @@
+//Nicholas Alexeev
+//purpose: define reverseList and LLMap
+//10-10-2018
+//licsened under gpl-v3
 #ifndef DP6
 #define DP6
 #include <algorithm> 
 #include "llnode2.h"
+//precondition: head is a valid uniqe_ptr to LLNode2
+//noexcept guarantee
 template<typename ValType>
 void reverseList(unique_ptr<LLNode2<ValType>> & head){
 	unique_ptr<LLNode2<ValType>> new_head=nullptr;
@@ -16,16 +22,22 @@ void reverseList(unique_ptr<LLNode2<ValType>> & head){
 	head = std::move(new_head);
 
 };
+//Basic Pair template
+//writtem because std::pair has a wierd api
 template<typename A,typename B>
-class Pair{
+struct Pair{
 	public:
 		Pair(const A& a,const B& b):_a(a),_b(b){};
 		A _a;
 		B _b;
 };
+//class invariants:_data has Pair, the key has operator== and the datatype has operator= 
 template<typename KeyType,typename DataType>
 class LLMap{
 	public:
+		//pre: none
+		//post: none as this does nothing
+		//noexcept guarantee
 		LLMap(){
 		}
 
@@ -39,6 +51,9 @@ class LLMap{
 		LLMap& operator=(LLMap&&)=delete;
 		LLMap& operator=(const LLMap&&)=delete;
 
+		//pre: none
+		//post: returns size
+		//noexcept guarantee
 		const size_t size()const{
 			auto current_data=_data.get();
 			if(current_data==nullptr){
@@ -54,26 +69,32 @@ class LLMap{
 				}
 			}
 		}
+		//pre: none
+		//post: returns true if empty
+		//noexcept guarantee
 		bool empty()const{
 			return _data==nullptr;	
 		};
+		//pre: none
+		//post: returns ptr to data
+		//noexcept guarantee
 		DataType* find(const KeyType &key_in){
 			auto current_data = _data.get();
 			while(0==0){
 				if(current_data==nullptr){
 					return nullptr;
 				}
-				if(current_data->_data._a==key_in){
+				else if(current_data->_data._a==key_in){
 					return &current_data->_data._b;
-				}
-				if(current_data->_next!=nullptr){
-					current_data=current_data->_next.get();
 				}else{
-					return nullptr;
+					current_data=current_data->_next.get();
 				}
 			}
 
 		}
+		//pre: none
+		//post: returns ptr to data
+		//noexcept guarantee
 		const DataType* find(const KeyType &key_in)const{
 			auto current_data = _data.get();
 			while(0==0){
@@ -82,9 +103,20 @@ class LLMap{
 				}
 				else if(current_data->_data._a==key_in){
 					return &current_data->_data._b;
+				}else{
+					current_data=current_data->_next.get();
 				}
+
 			}
 		}
+// Pre:
+//     head points to empty-ptr-terminated Linked List, or is empty.
+// Requirements on Types:
+//     ValType must have a copy ctor and a (non-throwing) dctor.
+// NOTE: The above are the requirements for LLNode2<ValType>; no member
+// functions of ValType are actually used here.
+// Strong guarantee
+// Exception neutral
 		void insert(const KeyType &key,const DataType &data){
 			DataType* found = find(key);
 			if(found==nullptr){
@@ -93,6 +125,9 @@ class LLMap{
 				*found=data;
 			}
 		}
+		//noexcept guarantee
+		//pre: key has operator==
+		//post: data with key is no longer present
 		void erase(const KeyType &key_in){
 			auto current_data = _data.get();
 			while(0==0){
@@ -111,6 +146,9 @@ class LLMap{
 				}
 			}
 		}
+		//strong guarantee
+		//exception neutral
+		//pre: fn has operator()
 		template <typename fn>
 		void traverse(fn lambda){
 			auto current_data = _data.get();
