599daa299a0e4059435e7483aafd4bea2f93899f
Nick <scifi6546@protonmail.com>
2019-12-02 10:38:12 -0900

    done with part A

--- HW08/followers.cpp
+++ HW08/followers.cpp
@@ -1,5 +1,56 @@
-#include <ifstream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
 
 int main(){
+	std::string fname;
+	std::cout<<"choose file:\n";
+	std::getline(std::cin,fname);
+	std::ifstream ifs (fname, std::ifstream::in);
+	std::map<std::string,std::vector<std::string>> dict;
+
+	if(ifs.good()){
+		
+		std::string previous="";
+		std::string current="";
+		std::cout<<"opened "<<fname<<"\n";
+		while(0==0){
+			if(ifs.eof()){
+				break;
+			}
+			current="";
+			ifs>>current;
+			if(ifs.eof()){
+				dict[current]=std::vector<std::string>();
+				break;
+			}
+			if(previous!=""){
+				bool in_vec=false;
+				for(auto item:dict[previous]){
+					if(current==item)
+						in_vec=true;
+				}
+				if(!in_vec)
+					dict[previous].push_back(current);
+			}
+			previous=current;
+		}
+
+	}else{
+		std::cout<<"error invalid file\n";
+		return 1;
+	}
+	for(auto i:dict){
+		std::cout<<i.first<<": ";
+		std::sort(i.second.begin(),i.second.end());
+		for(auto j:i.second){
+			std::cout<<j<<" ";
+		}
+		std::cout<<"\n";
+
+	}
 	return 0;
 }
